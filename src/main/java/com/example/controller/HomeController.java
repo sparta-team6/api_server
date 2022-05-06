5aa3b354fa674d73665ce35687cb1069397bd016
Jaeho Jung <iwogh3176@gmail.com>
2022-05-06 13:53:53 +0900

    프로퍼티 추가 카카오 테스트15

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -6,19 +6,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller
+@RestController
 @RequiredArgsConstructor
 public class HomeController {
 
+//    @GetMapping("/")
+//    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) User loginMember, Model model) {
+//        // 세션에 회원 데이터가 없으면 home
+//        if (loginMember == null) {
+//            return "index";
+//        }
+//        model.addAttribute("user", loginMember);
+//        return "index";
+//    }
+
     @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) User loginMember, Model model) {
-        // 세션에 회원 데이터가 없으면 home
-        if (loginMember == null) {
-            return "index";
-        }
-        model.addAttribute("user", loginMember);
-        return "index";
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 }
