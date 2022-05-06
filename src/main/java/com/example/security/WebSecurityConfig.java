cb94640ef4bddf7872c2d163b77f8b9151acb1c4
Jaeho Jung <iwogh3176@gmail.com>
2022-05-06 12:02:40 +0900

    프로퍼티 추가 카카오 테스트14

--- src/main/java/com/example/security/WebSecurityConfig.java
+++ src/main/java/com/example/security/WebSecurityConfig.java
@@ -33,42 +33,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-                .csrf().disable();
+        http.csrf().disable();
 
         http
-//                .cors().configurationSource(corsConfigurationSource())
-//                .and()
-                .httpBasic().disable() // 기본 로그인 페이지 사용 안함
-                // [로그인 기능]
-                .formLogin().disable()
-                // REST API 사용하기 때문에 사용 안함
+            .cors().configurationSource(corsConfigurationSource())
+            .and()
+            .httpBasic().disable() // 기본 로그인 페이지 사용 안함
+            // [로그인 기능]
+            .formLogin().disable()
+            // REST API 사용하기 때문에 사용 안함
 //                .csrf().ignoringAntMatchers("/api/**")
 //                .and()
-                .authorizeRequests() // ↓ 인증 인가 관련 ↓
-                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                    // image 폴더를 login 없이 허용
-                    .antMatchers("/images/**").permitAll()
-                    // css 폴더를 login 없이 허용
-                    .antMatchers("/css/**").permitAll()
-                    // 그 외 login 없이 허용하는 범위
-                    .antMatchers("/api/register").permitAll()  // 회원가입 페이지 이동
-                    .antMatchers("/").permitAll()  // 메인 페이지 이동
-                    .antMatchers("/api/login").permitAll() // 로그인
-                    .antMatchers("/user/kakao/callback/**").permitAll() // 로그인
-                    .antMatchers("/api/logout").permitAll() // 로그아웃
-                // 그 외 어떤 요청이든 '인증 필요'
-                .anyRequest().authenticated();
+            .authorizeRequests() // ↓ 인증 인가 관련 ↓
+                // PreFlight 요청 인증 X
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
+                // 그 외 login 없이 허용하는 범위
+                .antMatchers("/api/register").permitAll()  // 회원가입 페이지 이동
+                .antMatchers("/").permitAll()  // 메인 페이지 이동
+                .antMatchers("/api/login").permitAll() // 로그인
+                .antMatchers("/user/kakao/callback/**").permitAll() // 로그인
+                .antMatchers("/api/logout").permitAll() // 로그아웃
+            // 그 외 어떤 요청이든 '인증 필요'
+            .anyRequest().authenticated();
     }
 
     // CORS 정책 필터
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://3.36.112.56:8080");
+        configuration.addAllowedOrigin("http://localhost:8080");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
-//        configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
